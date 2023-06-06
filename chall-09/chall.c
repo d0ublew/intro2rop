966d2643e862fbb4a242a4b9fe43bc8cdfd3af43
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-06-06 10:55:30 +0800

    chore: revert back to old chall-09

--- chall-09/chall.c
+++ chall-09/chall.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char buf_g[32];
-
-void _main() {
-    __asm__("xchg %rsp, %rax");
-    __asm__("ret");
-    __asm__("pop %rax");
-    __asm__("ret");
-}
-
 void setup() {
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
@@ -20,8 +11,7 @@ void setup() {
 int main(void) {
     setup();
     write(1, "Welcome to intro2rop chall-08\nInput: ", 37);
-    read(0, buf_g, 256);
-    write(1, "\nInput 2: ", 10);
     char buf[32];
+    read(0, buf, 256);
     return 0;
 }
